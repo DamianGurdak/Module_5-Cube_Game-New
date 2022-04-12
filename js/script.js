38efd9508f08f1a96dc9679c0e37c0dde2c4483e
Damian Gurdak <damiangurdakk@gmail.com>
2022-04-12 20:43:31 +0200

    Add code cube game

--- js/script.js
+++ js/script.js
@@ -1,21 +1,49 @@
-function printMessage(msg) {
-	let div = document.createElement("div");
-	div.innerHTML = msg;
-	document.getElementById("messages").appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+//
+console.log("Wylosowana liczba to: " + randomNumber);
+//
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+	computerMove = "kamień";
+} else if (randomNumber == 2) {
+	computerMove = "papier";
+} else if (randomNumber == 3) {
+	computerMove = "nożyce";
 }
 
-function clearMessages() {
-	document.getElementById("messages").innerHTML = "";
+//printMessage("Mój ruch to: " + computerMove);
 
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+
+//
+console.log("Gracz wpisał: " + playerInput);
+//
+
+let playerMove = "nieznany ruch";
+
+if (playerInput == "1") {
+	playerMove = "kamień";
+} else if (playerInput == "2") {
+	playerMove = "papier";
+} else if (playerInput == "3") {
+	playerMove = "nożyce";
 }
 
-let computerMove = kamień;
-let playerMove = papier;
-
-printMessage(
-	"Zagrałem" +
-		computerMove +
-		"! Jeśli tówj ruch to" +
-		playerMove +
-		"to wygrałes"
-);
+if (
+	(computerMove == "kamień" && playerMove == "papier") ||
+	(computerMove == "papier" && playerMove == "nożyce") ||
+	(computerMove == "nożyce" && playerMove == "kamień")
+) {
+	printMessage("Wygrał gracz!");
+} else if (
+	(playerMove == "kamień" && computerMove == "papier") ||
+	(playerMove == "papier" && computerMove == "nożyce") ||
+	(playerMove == "nożyce" && computerMove == "kamień")
+) {
+	printMessage("Wygarł komputer!");
+} else if (playerMove == "nieznany ruch" || computerMove == "nieznany ruch") {
+	printMessage("Nie znany ruch. Powtórz ruch!");
+}
